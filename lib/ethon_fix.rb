cbcc60a16ec585b8838d6f1306c491f3cd04660d
Howard Ding <hding2@illinois.edu>
2013-03-05 17:23:22 -0600

    Another try at fixing this.

--- lib/ethon_fix.rb
+++ lib/ethon_fix.rb
@@ -1,8 +1,8 @@
 require 'typhoeus'
 
 module Ethon
-  module Easy
-    class Options
+  class Easy
+    module Options
       def unrestricted_auth=(value)
         Curl.set_option(:unrestricted_auth, value_for(value, :bool), handle)
       end
